93278d2b6f1094da337a9bd8435a71e11c737e9e
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2018-02-02 15:18:57 +0100

    OLMIS-4025: Updated RAML and schema. Also added RequisitionTemplateAssignment domain and migration

---
+++ src/main/resources/db/migration/20180202145705498__created_requisition_template_assignments_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE requisition_template_assignments (
+    id UUID PRIMARY KEY NOT NULL,
+    programid uuid NOT NULL,
+    facilitytypeid uuid NOT NULL,
+    templateid uuid NOT NULL
+);
+
+ALTER TABLE ONLY requisition_template_assignments
+    ADD CONSTRAINT requisition_template_assignment_unique_program_facility_type UNIQUE (facilitytypeid, programid);
