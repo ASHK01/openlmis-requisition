0c31de23eee3c72277d7e4d5fa0fe2f8a164d9fb
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2018-02-05 11:24:58 +0100

    OLMIS-4025 FEOLMIS-2459: Updated unique constraint

--- src/main/resources/db/migration/20180202145705498__created_requisition_template_assignments_table.sql
+++ src/main/resources/db/migration/20180202145705498__created_requisition_template_assignments_table.sql
@@ -6,4 +6,4 @@ CREATE TABLE requisition_template_assignments (
 );
 
 ALTER TABLE ONLY requisition_template_assignments
-    ADD CONSTRAINT requisition_template_assignment_unique_program_facility_type UNIQUE (facilitytypeid, programid);
+    ADD CONSTRAINT requisition_template_assignment_unique_program_facility_type_template UNIQUE (facilitytypeid, programid, templateid);
